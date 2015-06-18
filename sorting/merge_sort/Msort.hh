00b145f14c32036caa39ae1b4d05bab15d565e54
Eddy Zagabe <zagabe.ed@gmail.com>
2015-06-19 01:21:38 +0200

    Init commit merge sort

---
+++ sorting/merge_sort/Msort.hh
@@ -0,0 +1,16 @@
+// Author : zagabe.ed@gmail.com (Eddy ZAGABE)
+//
+// This is an implementation of the merge sort algorithm. He's
+// represented by the class Msort.
+// The merge sort use an algorithm design paradigm based on recursion
+// (See README.md for more information)
+//
+// Average case : O(n log n)
+// Worst case space complexity : O(n)
+
+#ifndef MSORT_HH_
+#define MSORT_HH_
+
+class Msort {};
+
+#endif  // MSORT_HH_
