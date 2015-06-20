627cf81dc24ad33827755f020a853e9b8f76139e
Eddy Zagabe <zagabe.ed@gmail.com>
2015-06-20 02:55:28 +0200

    Merge sort implementation

--- sorting/merge_sort/Msort.hh
+++ sorting/merge_sort/Msort.hh
@@ -11,6 +11,83 @@
 #ifndef MSORT_HH_
 #define MSORT_HH_
 
-class Msort {};
+#include <vector>
+#include <cmath> // for floor()
+
+class Msort {
+ public:
+  // Constructor
+  Msort() {}
+
+  // Destructor
+  ~Msort() {}
+
+  // Sorting member function that calls the recursive function
+  void sort(std::vector<int> &array) {
+    Msort::mergeSort(array, 0, array.size() - 1);
+  }
+
+ private:
+  // Merge two subarrays
+  void merge(std::vector<int> &array, int p, int q, int r);
+
+  // Execute the merge sort algorithm recursively
+  void mergeSort(std::vector<int> &array, int p, int r);
+};
+
+// Merge function
+void Msort::merge(std::vector<int> &array, int p, int q, int r) {
+  std::vector<int> lowHalf;
+  std::vector<int> highHalf;
+  int c, cLowHalf, cHighHalf;
+
+  // Set the index to the begining of the entire subarray[p...r]
+  c = p;
+
+  // Initialize the low half of the subarray[p...q]
+  for (; c <= q; c++) {
+    lowHalf.push_back(array[c]);
+  }
+
+  // Initialize the high half of the subarray[q + 1...r]
+  for (; c <= r; c++) {
+    highHalf.push_back(array[c]);
+  }
+
+  c = p;
+  cLowHalf = 0;
+  cHighHalf = 0;
+
+  // Loop that compare the lowHalf and the highHalf and place the
+  // item in good order
+  for (; cLowHalf < lowHalf.size() && cHighHalf < highHalf.size();) {
+    if (lowHalf[cLowHalf] < highHalf[cHighHalf])
+      array[c++] = lowHalf[cLowHalf++];
+    else
+      array[c++] = highHalf[cHighHalf++];
+  }
+
+  // Add the rest of the low half part
+  for (; cLowHalf < lowHalf.size(); cLowHalf++) {
+    array[c++] = lowHalf[cLowHalf];
+  }
+
+  // Add the rest of the high half part
+  for (; cHighHalf < highHalf.size(); cHighHalf++) {
+    array[c++] = highHalf[cHighHalf];
+  }
+}
+
+// Merge sort's recursive function
+void Msort::mergeSort(std::vector<int> &array, int p, int r) {
+  int q;
+
+  if (p < r) { // Base case
+    q = floor((p + r) / 2); // the result is rounded down
+    Msort::mergeSort(array, p, q);
+    Msort::mergeSort(array, q + 1, r);
+    Msort::merge(array, p, q, r);
+  }
+}
 
 #endif  // MSORT_HH_
